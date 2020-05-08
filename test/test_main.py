d918d22a36c5a3eff633b00dea9df50552db2dc3
dianagamedi <da.gamez.diaz@gmail.com>
2020-05-08 14:03:57 +0200

    Test sad path when there are no students enrolled on a specific course

--- test/test_main.py
+++ test/test_main.py
@@ -10,15 +10,27 @@ test_schema = StructType([
 ])
 
 
-def test_machine_learning_students(spark_session):
+def test_return_only_students_enrolled_on_specific_course(spark_session):
     data_frame = spark_session.createDataFrame([
         [1, "Diana", "Machine Learning", "2020-05"],
         [2, "Jose", "Machine Learning", "2020-04"],
         [3, "Sebastian", "Engineering", "2020-05"]
     ], test_schema)
 
-    machine_learning_students = students_per_course(data_frame, "Machine Learning").collect()
+    students = students_per_course(data_frame, "Machine Learning").collect()
 
-    assert len(machine_learning_students) == 2
-    assert machine_learning_students[0]["name"] == "Diana"
-    assert machine_learning_students[1]["name"] == "Jose"
+    assert len(students) == 2
+    assert students[0]["name"] == "Diana"
+    assert students[1]["name"] == "Jose"
+
+
+def test_returns_zero_when_there_are_no_students_enrolled_on_a_course(spark_session):
+    data_frame = spark_session.createDataFrame([
+        [1, "Diana", "Machine Learning", "2020-05"],
+        [2, "Jose", "Machine Learning", "2020-04"],
+        [3, "Sebastian", "Engineering", "2020-05"]
+    ], test_schema)
+
+    students = students_per_course(data_frame, "Data Science").collect()
+
+    assert len(students) == 0
